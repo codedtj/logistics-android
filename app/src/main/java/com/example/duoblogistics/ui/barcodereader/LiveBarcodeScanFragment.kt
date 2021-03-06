3f849c8d4074919e34b2bba75f2be1395683fa5a
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-03-06 22:33:39 +0500

    import

---
+++ app/src/main/java/com/example/duoblogistics/ui/barcodereader/LiveBarcodeScanFragment.kt
@@ -0,0 +1,18 @@
+package com.example.duoblogistics.ui.barcodereader
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.duoblogistics.R
+
+class LiveBarcodeScanFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_live_barcode_scan, container, false)
+    }
+}
