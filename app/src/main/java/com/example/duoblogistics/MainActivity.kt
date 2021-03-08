eb6ea1edc91c72abbdca5bd5b45ba2fe0254c6bd
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-03-08 15:37:37 +0500

    camera permissions

--- app/src/main/java/com/example/duoblogistics/MainActivity.kt
+++ app/src/main/java/com/example/duoblogistics/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.duoblogistics
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private val CAMERA_PERMISSION_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +35,32 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+    }
+
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            )
+            == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat
+                .requestPermissions(
+                    this@MainActivity, arrayOf(permission),
+                    requestCode
+                )
+        } else {
+            Toast
+                .makeText(
+                    this@MainActivity,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT
+                )
+                .show()
+        }
     }
 }
