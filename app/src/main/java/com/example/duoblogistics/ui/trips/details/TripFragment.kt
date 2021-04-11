69eb36088252275a880911ea5e99f03605903460
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-04-11 19:15:28 +0500

    launch scanner from fragment

--- app/src/main/java/com/example/duoblogistics/ui/trips/details/TripFragment.kt
+++ app/src/main/java/com/example/duoblogistics/ui/trips/details/TripFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.duoblogistics.R
 import com.example.duoblogistics.databinding.FragmentTripBinding
+import com.example.duoblogistics.ui.barcodereader.LiveBarcodeScanFragment
 import com.example.duoblogistics.ui.main.AppViewModel
 import com.example.duoblogistics.ui.main.AppViewModelFactory
 import com.example.duoblogistics.ui.trips.TripsViewModel
@@ -32,6 +36,8 @@ class TripFragment : Fragment(), KodeinAware {
     private val tripsViewModelFactory: TripsViewModelFactory by instance()
 
     private val appViewModelFactory: AppViewModelFactory by instance()
+
+    private var barCodeScannerIsVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,6 +89,31 @@ class TripFragment : Fragment(), KodeinAware {
                     "Отсканировано " + storedItems.count { it.scanned }.toString()
             }
         })
+
+        binding.toogleScannerButton.setOnClickListener {
+            barCodeScannerIsVisible = !barCodeScannerIsVisible
+
+            activity?.apply {
+                if (barCodeScannerIsVisible)
+                    supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+                        add<LiveBarcodeScanFragment>(
+                            R.id.tripFragmentContainerView,
+                            "Live_Scan_Barcode_Trip_Fragment"
+                        )
+                    }
+                else {
+                    val fragment =
+                        supportFragmentManager.findFragmentByTag("Live_Scan_Barcode_Trip_Fragment")
+                    if (fragment != null)
+                        supportFragmentManager.commit {
+                            setReorderingAllowed(true)
+                            remove(fragment)
+                        }
+                }
+
+            }
+        }
     }
 
     override fun onDestroyView() {
