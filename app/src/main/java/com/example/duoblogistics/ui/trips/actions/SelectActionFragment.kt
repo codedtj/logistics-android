88d05d7197831ce21f7cf6d65804a07091753b9a
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-04-12 23:01:56 +0500

    select action

---
+++ app/src/main/java/com/example/duoblogistics/ui/trips/actions/SelectActionFragment.kt
@@ -0,0 +1,44 @@
+package com.example.duoblogistics.ui.trips.actions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.duoblogistics.R
+import com.example.duoblogistics.databinding.FragmentSelectActionBinding
+import com.example.duoblogistics.databinding.FragmentTripBinding
+import com.example.duoblogistics.ui.trips.TripsViewModel
+import com.example.duoblogistics.ui.trips.TripsViewModelFactory
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
+
+class SelectActionFragment : Fragment(), KodeinAware {
+    override val kodein by closestKodein()
+
+    private lateinit var binding: FragmentSelectActionBinding
+
+    private lateinit var tripsViewModel: TripsViewModel
+
+    private val tripsViewModelFactory: TripsViewModelFactory by instance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        tripsViewModel = activity?.run {
+            ViewModelProvider(this, tripsViewModelFactory)
+                .get(TripsViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSelectActionBinding.inflate(inflater)
+        return binding.root
+    }
+}
